690018bff6cd09126fc628f5e05232650abbfccd
Andres Santamarina <andresesantamarina@gmail.com>
2023-11-01 11:32:45 -0300

    practica de arrays

---
+++ js/05-arrays.js
@@ -0,0 +1,62 @@
+// declarar un array
+const productos = [];
+
+const juegos = ['counter-strike', 'Minecraft', 2023, true, 'valorant', 'half-life', 'lol'];
+
+//mostrar un array
+console.log(juegos.length);
+console.log(juegos);
+document.write(juegos);
+
+//mostrar un solo elemento
+document.write(`<p>El primer juego es ${juegos[0]}</p>`);
+//por si no me acuerdo cuál es el último elemento
+document.write(`<p>El último juego es ${juegos[juegos.length - 1]}</p>`);
+//si pido una posición que no existe, me devuelve undefined, porque el array puede seguir creciendo
+document.write(`<p>Juego de la posición 20 ${juegos[20]}</p>`);
+
+document.write(`<h2>Lista de juegos</h2>`);
+document.write(`<ul>`);
+for (let indiceJuego = 0; indiceJuego < juegos.length; indiceJuego++) {
+    document.write(`<li>${juegos[indiceJuego]}</li>`);
+}
+document.write(`</ul>`);
+
+//agregar elementos al principio del array
+juegos.unshift('God of War', 'Street Fighter');
+document.write(`<h2>Lista de juegos + 2 juegos extras</h2>`);
+document.write(`<ul>`);
+for (let indiceJuego = 0; indiceJuego < juegos.length; indiceJuego++) {
+    document.write(`<li>${juegos[indiceJuego]}</li>`);
+}
+document.write(`</ul>`);
+
+console.log(juegos.length);
+console.log(juegos);
+
+//agregar elementos en el medio
+juegos.splice(6, 0, 'Dark Souls');
+document.write(`<h2>Lista de juegos + 1, cantidad total (${juegos.length})</h2>`);
+document.write(`<ul>`);
+for (let indiceJuego = 0; indiceJuego < juegos.length; indiceJuego++) {
+    document.write(`<li>${juegos[indiceJuego]}</li>`);
+}
+document.write(`</ul>`);
+
+console.log(juegos.length);
+console.log(juegos);
+
+//agregar elementos al final
+juegos.push('Mortal Kombat');
+document.write(`<h2>Lista de juegos + 1, cantidad total (${juegos.length})</h2>`);
+document.write(`<ul>`);
+for (let indiceJuego = 0; indiceJuego < juegos.length; indiceJuego++) {
+    document.write(`<li>${juegos[indiceJuego]}</li>`);
+}
+document.write(`</ul>`);
+
+console.log(juegos.length);
+console.log(juegos);
+
+
+
